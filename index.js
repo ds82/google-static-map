15129d0fbced986d64d400a77be878fcd7a11562
Dennis Sänger <github@mail.ds82.de>
2014-09-25 15:21:22 +0200

    first version of google-static-map

---
+++ index.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var request    = require('request');
+var gm         = require('googlemaps');
+
+var GoogleStaticMap = function( consoleKey, opts ) {
+  opts = opts || {};
+  if ( !consoleKey ) {
+    throw new Error('You must provide a google api console key');
+  }
+
+  gm.config('console-key', consoleKey );
+
+  this.values = {};
+  this.values.last = null;
+  this.values.url = null;
+
+  this.config = {};
+  this.config.style = {
+    feature: 'road',
+    element: 'all',
+    rules: {
+      hue: '0x00ff00'
+    }
+  };
+
+  this.config.markers = [{
+    'location': '',
+    'color': 'blue',
+    'label': '',
+    'shadow': 'false'
+  }];
+
+  this.config.paths = [];
+
+  this.config.zoom = 15;
+  this.config.resolution = '480x300';
+  this.config.maptype = 'roadmap';
+
+  this.config.address = null;
+
+  //
+  // config getter ./. setter
+  //
+
+  var genericConfigHandler = (function ( configKey, value ) {
+    if ( !value ) { return this.config[configKey]; }
+    else {
+      this.config[configKey] = value;
+      return this;
+    }
+  }).bind( this );
+
+  this.style = function( style ) {
+    return genericConfigHandler( 'style', style );
+  };
+
+  this.markers = function( markers ) {
+    return genericConfigHandler( 'markers', markers );
+  };
+
+  this.zoom = function( zoom ) {
+    return genericConfigHandler( 'zoom', zoom );
+  };
+
+  this.resolution = function( resolution ) {
+    return genericConfigHandler( 'resolution', resolution );
+  };
+
+  this.mapType = function( mapType ) {
+    return genericConfigHandler( 'mapType', mapType );
+  };
+
+  this.address = function( address ) {
+    if ( address ) {
+      this.config.markers[0].location = address;
+    }
+    return genericConfigHandler( 'address', address );
+  };
+
+  //
+  // methods
+  //
+
+  this.done = function() {
+    return this.values.last;
+  };
+
+  this.url = function() {
+    this.values.last = this.values.url = gm.staticMap( this.config.address,
+      this.config.zoom,
+      this.config.resolution,
+      function(){},
+      false,
+      this.config.mapType,
+      this.config.markers,
+      this.config.style,
+      this.config.paths
+    );
+    return this;
+  };
+
+  this.staticMap = function() {
+    if ( ! this.values.url ) { this.url(); }
+    this.values.last = request( this.values.url );
+    return this;
+  };
+};
+
+module.exports = function( consoleKey, opts ) {
+  console.log( 'consoleKey', consoleKey );
+  return new GoogleStaticMap( consoleKey, opts );
+};
+
+module.exports.set = function( key ) {
+  var consoleKey = key;
+  return function( opts ) {
+    return new GoogleStaticMap( consoleKey, opts );
+  };
+};
+
+
+
---
+++ test.spec.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var proxy = require('proxyquire');
+var stubs = {
+  googlemaps:    jasmine.createSpyObj('googlemaps', ['staticMap']),
+  request:       jasmine.createSpy('request'),
+  '@noCallThru': true
+};
+
+describe('google-static-map', function() {
+
+  var uut;
+
+  describe('without auto-setting a key', function() {
+
+    beforeEach(function() {
+      uut = proxy('./index', stubs );
+    });
+
+    it('should not work without a api key', function() {
+      expect( uut ).toThrow(new Error('You must provide a google api console key'));
+    });
+
+    it('should provide a method to set a global api key', function() {
+      expect( uut.set ).toBeDefined();
+      uut = uut.set('some-key');
+      expect( uut ).not.toThrow( jasmine.any( Error ));
+    });
+
+  });
+
+  describe('with auto-setting a key', function() {
+
+    beforeEach(function() {
+      uut = proxy('./index', stubs ).set('some-key');
+    });
+
+    it('should get/set config options', function() {
+      var map = uut();
+      var set;
+
+      ['zoom', 'resolution', 'mapType', 'markers', 'style'].forEach(function( key ) {
+        set = key + ' ' + key;
+        expect( map.config[key] ).toEqual( map[key]() );
+        var chain = map[key]( set );
+        expect( map.config[key] ).toEqual( set );
+
+        expect( chain ).toEqual( map );
+      });
+    });
+
+    it('should relay staticMap call to googlemaps module', function() {
+      var map = uut();
+
+      var testAddress = 'Some Address, Some Country';
+      var staticMapReturn = 'http://some.where';
+      var requestReturn = 'request-return-value';
+
+      stubs.googlemaps.staticMap.andReturn( staticMapReturn );
+      stubs.request.andReturn( requestReturn );
+
+      var stream = map.address( testAddress ).staticMap().done();
+
+      expect( stream ).toEqual('request-return-value');
+      expect( stubs.googlemaps.staticMap ).toHaveBeenCalledWith(
+        testAddress,
+        map.config.zoom,
+        map.config.resolution,
+        jasmine.any( Function ),
+        false,
+        map.config.mapType,
+        map.config.markers,
+        map.config.style,
+        map.config.paths
+      );
+      expect( stubs.request ).toHaveBeenCalledWith( staticMapReturn );
+    });
+
+  });
+
+
+
+});
